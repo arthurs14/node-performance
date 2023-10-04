a8e375d778dd971a1b5156adafc0d5f7e6f7fa5b
arthurs14 <arthurcs426@gmail.com>
2023-10-04 15:32:51 -0700

    clustering example

--- server.js
+++ server.js
@@ -1,27 +1,34 @@
 const express = require('express');
+const cluster = require('cluster');
 
 const PORT = 8000;
 
 const app = express();
 
-
-
 function delay(duration) {
   const startTime = Date.now();
 
-  while(Date.now() - startTime < duration) {
+  while (Date.now() - startTime < duration) {
     // event loop is blocked...
   }
 }
 
 app.get('/', (req, res) => {
-  res.send('Performance Example');
+  res.send(`Performance Example: ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
   // delay the response
   delay(9000);
-  res.send('Ding ding ding!');
+  res.send(`Ding ding ding! ${process.pid}`);
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+console.log('Running server.js...\n');
+if (cluster.isMaster) {
+  console.log('master has been started...');
+  cluster.fork();
+  cluster.fork();
+} else {
+  console.log('Worker process started.');
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+}
