fabc13d45fd00b529161ead4b7d8f913c85bab73
arthurs14 <arthurcs426@gmail.com>
2023-10-04 16:24:26 -0700

    changed delay to 4 seconds

--- server.js
+++ server.js
@@ -18,8 +18,9 @@ app.get('/', (req, res) => {
 
 app.get('/timer', (req, res) => {
   // delay the response
-  delay(9000);
-  res.send(`Ding ding ding! ${process.pid}`);
+  delay(4000);
+  // res.send(`Ding ding ding! ${process.pid}`);
+  res.send(`Beep beep beep! ${process.pid}`);
 });
 
 console.log('Running server.js...');
